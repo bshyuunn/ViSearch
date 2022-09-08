c712e6dcfded81ee52d287e4eddb1180215ca6ea
Otwooo <shyunb21@gmail.com>
2022-09-08 20:04:39 +0900

    feat: Add crawling filter

--- util/crawling.py
+++ util/crawling.py
@@ -4,38 +4,62 @@ import csv
 import pandas as pd
 
 # 검색어 결과 제목 및 링크 크로링하기
-def craw_title_href(num):
+def craw_title_href(num, news):
     global craw_df
     global href_data
     global search
 
     options = webdriver.ChromeOptions()
-    options.add_argument('headless')
+    # options.add_argument('headless')
     options.add_argument('window-size=1920x1080')
     options.add_argument("disable-gpu")
     
     options.add_experimental_option("excludeSwitches", ["enable-logging"])
 
-    driver = webdriver.Chrome('./chromedriver', options=options)
+    driver = webdriver.Chrome('./util/chromedriver', options=options)
 
     url = 'https://www.google.com/search?q='
     url += search
-    url += '&source=lmns&tbm=nws' # 뉴스 페이지에서 검색
-    url += '&start='
-    url += str(num) + str(0)
-    
-    driver.get(url)
-    
-    html = driver.page_source
-    soup = BeautifulSoup(html)
-    
-    title = soup.select('.xuvV6b.BGxR7d')        
-    
-    for i in title:      
-        craw_df = craw_df.append({'title' : i.select_one('.mCBkyc.y355M.ynAwRc.MBeuO.nDgy9d').text}, ignore_index=True)
-        href_data.append(i.a.attrs['href'])
+        
+    # 뉴스 페이지에서 검색
+    if news == 1:        
+        url += '&source=lmns&tbm=nws' 
 
-    driver.close()
+        url += '&start='
+        url += str(num) + str(0)
+        
+        driver.get(url)
+        
+        html = driver.page_source
+        soup = BeautifulSoup(html)
+        
+        title = soup.select('.xuvV6b.BGxR7d')             
+        
+        for i in title:      
+            craw_df = craw_df.append({'title' : i.select_one('.mCBkyc.y355M.ynAwRc.MBeuO.nDgy9d').text}, ignore_index=True)
+            href_data.append(i.a.attrs['href'])
+
+        driver.close()
+    
+    # 전체 페이지에서 검색
+    else:        
+        url += '&start='
+        url += str(num) + str(0)
+        
+        driver.get(url)
+        
+        html = driver.page_source
+        soup = BeautifulSoup(html)
+        
+        title = soup.select('.kvH3mc.BToiNc.UK95Uc')  
+                
+        
+        for i in title:      
+            craw_df = craw_df.append({'title' : i.select_one('.LC20lb.MBeuO.DKV0Md').text}, ignore_index=True)
+            href_data.append(i.a.attrs['href'])
+
+        driver.close()
+
 
 # 수집한 링크의 본문 글까지 크롤링하기
 def additional_craw(href_data):
@@ -48,7 +72,7 @@ def additional_craw(href_data):
     
     options.add_experimental_option("excludeSwitches", ["enable-logging"])
 
-    driver = webdriver.Chrome('./chromedriver', options=options)
+    driver = webdriver.Chrome('./util/chromedriver', options=options)
 
     for i in range(len(href_data)):
         url = href_data[i]
@@ -74,10 +98,11 @@ href_data = []
 
 # 검색어 입력받기
 search = input('검색어를 입력하시요 : ')
+news = int(input("\n전체 페이지 검색 : 0 \n뉴스 페이지에서 검색 : 1\n입력 : "))
 
 # 페이지 10개 크로링
 for i in range(1):
-    craw_title_href(i)
+    craw_title_href(i, news)
 
 # title의 링크에 들어가 본문 크롤링
 additional_craw(href_data)
