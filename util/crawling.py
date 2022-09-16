c95e04813402a9b1a3030e5623cb3a4be790a518
Otwooo <shyunb21@gmail.com>
2022-09-16 23:28:33 +0900

    fix: Fix bug

--- util/crawling.py
+++ util/crawling.py
@@ -16,62 +16,66 @@ def craw_title_href(num, news, search):
     global craw_df
     global href_data    
 
-    options = webdriver.ChromeOptions()
-    options.add_argument('headless')
-    options.add_argument('window-size=1920x1080')
-    options.add_argument("disable-gpu")
-    
-    options.add_experimental_option("excludeSwitches", ["enable-logging"])
+    try:
 
-    driver = webdriver.Chrome('./util/chromedriver', options=options)
+        options = webdriver.ChromeOptions()
+        options.add_argument('headless')
+        options.add_argument('window-size=1920x1080')
+        options.add_argument("disable-gpu")
+        
+        options.add_experimental_option("excludeSwitches", ["enable-logging"])
 
-    url = 'https://www.google.com/search?q='
-    url += search
-        
-    # 뉴스 페이지에서 검색
-    if news == 1:        
-        url += '&source=lmns&tbm=nws' 
+        driver = webdriver.Chrome('./util/chromedriver', options=options)
 
-        url += '&start='
-        url += str(num) + str(0)
-        
-        driver.get(url)
-        
-        html = driver.page_source
-        soup = BeautifulSoup(html)
-        
-        title = soup.select('.xuvV6b.BGxR7d')             
-        
-        for i in title:      
-            craw_df = craw_df.append({'title' : i.select_one('.mCBkyc.y355M.ynAwRc.MBeuO.nDgy9d').text}, ignore_index=True)
-            href_data.append(i.a.attrs['href'])
+        url = 'https://www.google.com/search?q='
+        url += search
+            
+        # 뉴스 페이지에서 검색
+        if news == 1:        
+            url += '&source=lmns&tbm=nws' 
 
-        driver.close()
-    
-    # 전체 페이지에서 검색
-    else:        
-        url += '&start='
-        url += str(num) + str(0)
-        
-        driver.get(url)
-        
-        html = driver.page_source
-        soup = BeautifulSoup(html)
-        
-        title = soup.select('.kvH3mc.BToiNc.UK95Uc')  
-                
-        
-        for i in title:      
-            craw_df = craw_df.append({'title' : i.select_one('.LC20lb.MBeuO.DKV0Md').text}, ignore_index=True)
-            href_data.append(i.a.attrs['href'])
+            url += '&start='
+            url += str(num) + str(0)
+            
+            driver.get(url)
+            
+            html = driver.page_source
+            soup = BeautifulSoup(html)
+            
+            title = soup.select('.xuvV6b.BGxR7d')             
+            
+            for i in title:      
+                craw_df = craw_df.append({'title' : i.select_one('.mCBkyc.y355M.ynAwRc.MBeuO.nDgy9d').text}, ignore_index=True)
+                href_data.append(i.a.attrs['href'])
 
-        driver.close()
+            driver.close()
+        
+        # 전체 페이지에서 검색
+        else:        
+            url += '&start='
+            url += str(num) + str(0)
+            
+            driver.get(url)
+            
+            html = driver.page_source
+            soup = BeautifulSoup(html)
+            
+            title = soup.select('.kvH3mc.BToiNc.UK95Uc')  
+                    
+            
+            for i in title:      
+                craw_df = craw_df.append({'title' : i.select_one('.LC20lb.MBeuO.DKV0Md').text}, ignore_index=True)
+                href_data.append(i.a.attrs['href'])
+
+            driver.close()
+    except: 
+        return 0
 
 
 # 수집한 링크의 본문 글까지 크롤링하기
 def additional_craw(href_data):
     global craw_df
-
+    
     options = webdriver.ChromeOptions()
     options.add_argument('headless')
     options.add_argument('window-size=1920x1080')
@@ -82,22 +86,25 @@ def additional_craw(href_data):
     driver = webdriver.Chrome('./util/chromedriver', options=options)
 
     for i in range(len(href_data)):
-        url = href_data[i]
-        driver.get(url)
+        try:
+            url = href_data[i]
+            driver.get(url)
 
-        html = driver.page_source
-        soup = BeautifulSoup(html)
+            html = driver.page_source
+            soup = BeautifulSoup(html)
 
-        text = soup.select('p')
-        temp = ''
+            text = soup.select('p')
+            temp = ''
 
-        for j in text:
-            temp += j.text
-        craw_df.loc[i, 'text'] = temp    
-        
-        print("=======================================")
-        print('2차 크롤링 중', f'{i+1}/{len(href_data)}' )
-        print("=======================================")   
+            for j in text:
+                temp += j.text
+            craw_df.loc[i, 'text'] = temp    
+            
+            print("=======================================")
+            print('2차 크롤링 중', f'{i+1}/{len(href_data)}' )
+            print("=======================================")   
+        except:
+            continue
 
     print("=======================================")
     print('2차 크롤링 완료!')
