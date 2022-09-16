e1f368b366bdadc063834da8ddda6b27cfcaadd7
Otwooo <shyunb21@gmail.com>
2022-09-16 22:24:10 +0900

    style: Modify search page

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+import os
 
 # 크롤링 함수
 from util.crawling import Search
@@ -17,11 +18,15 @@ def search():
     search = request.args.get('search')
     news = int(request.args.get('news'))
     Data_Collection = int(request.args.get('Data_Collection'))
-    
-    Search(search, news, Data_Collection)        
-    wordcloudword = wordclouod_word(search)    
 
-    return render_template('search.html', word_list = wordcloudword)
+    dir_path = f"./craw_data/{search}.csv"
+    
+    if os.path.isfile(dir_path) == False:
+        Search(search, news, Data_Collection)        
+    
+    wordcloudword = wordclouod_word(search)
+
+    return render_template('search.html', word_list = wordcloudword, keyword = search)
 
 if __name__ == '__main__':    
     app.run()
